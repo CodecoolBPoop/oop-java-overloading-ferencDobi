37bb4915d9c8ed57bc9ef08890e2dcfaace633bf
ferencDobi <ferencdbi@gmail.com>
2018-08-10 19:14:38 +0200

    supplier skeleton

--- src/com/codecool/uml/overloading/Supplier.java
+++ src/com/codecool/uml/overloading/Supplier.java
@@ -1,4 +1,49 @@
 package com.codecool.uml.overloading;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Supplier {
+    private int id;
+    private String name;
+    private String description;
+
+    public Supplier() {
+        // TODO
+    }
+
+    public Supplier(String name, String description) {
+        // TODO
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        // TODO
+        return new LinkedList<Product>();
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        return super.toString();
+    }
 }
