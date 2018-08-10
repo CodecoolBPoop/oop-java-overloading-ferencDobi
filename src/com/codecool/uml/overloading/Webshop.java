a033ac48cb3bea76744a15ab3ae94c7c349d7984
ferencDobi <ferencdbi@gmail.com>
2018-08-10 20:32:22 +0200

    tests implement

--- src/com/codecool/uml/overloading/Webshop.java
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -1,8 +1,25 @@
 package com.codecool.uml.overloading;
 
+import java.util.Currency;
+import java.util.Date;
+
 public class Webshop {
 
     public static void main(String[] args) {
-	// write your code here
+        Product prod = new Product();
+        Product prod2 = new Product("name", 12.5F, Currency.getInstance("HUF"));
+        Supplier supp = new Supplier();
+        Supplier supp2 = new Supplier("name", "desc");
+        ProductCategory prodcat = new ProductCategory();
+        ProductCategory prodcat2 = new ProductCategory("name", "dept", "desc");
+        FeaturedProductCategory prodcat3 = new FeaturedProductCategory();
+        prodcat3.schedule(new Date(), new Date());
+        System.out.println(prod.toString());
+        System.out.println(prod2.toString());
+        System.out.println(supp.toString());
+        System.out.println(supp2.toString());
+        System.out.println(prodcat.toString());
+        System.out.println(prodcat2.toString());
+        System.out.println(prodcat3.toString());
     }
 }
